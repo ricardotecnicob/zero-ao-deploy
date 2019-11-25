dd2ec70066b1b6df7794409d4eaf495903877a66
Ricardo Alves <ricardotecnicoeletronica@gmail.com>
2019-11-25 15:43:54 -0300

    Node

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+
+const Router = require('koa-router');
+
+const app = new Koa();
+
+const router = new Router();
+
+const port = '3333';
+
+
+const index = ctx => {
+    ctx.body = 'zero-ao-deploy'
+}
+
+const hello = ctx => {
+    const name =  ctx.params.name || 'word'
+    ctx.body = ""
+}
+
+router.get('/', index)
+router.get('/hello', hello)
+router.get('/hello/:name', hello)
+
+app.use(router.routes())
+
+app.listen(port).on('listening', () => console.log(`listeng on port ${port}`))
